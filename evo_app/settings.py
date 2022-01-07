faf69251d869aabd176f6bf4012e9b49e50cc45f
darkmatter4114 <darkmatter4114@gmail.com>
2022-01-07 14:11:54 +0200

    edit config

--- evo_app/settings.py
+++ evo_app/settings.py
@@ -129,6 +129,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
